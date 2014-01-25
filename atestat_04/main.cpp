1d7ea5243400284680616f6058ab49b5be05fafe
AlexaGh <alexa.ghiurau@gmail.com>
2014-01-25 20:20:32 +0200

    #4.1

--- atestat_04/main.cpp
+++ atestat_04/main.cpp
@@ -2,12 +2,12 @@
 #include <fstream>
 using namespace std;
 int v[20],s[20],n;
-void afisare_b(int v[], int n)
+void afisare_b(int v2[], int n)
 {
     int x, ok, ok1;
     ok1=0; //pp "NU EXISTA"
-        for(int i=0;i<=n;i++)
-        {
+    for(int i=0; i<n; i++)
+    {
         x=v[i];
         ok=1; //pp x are toate cifrele pare
         while(x)
@@ -27,46 +27,48 @@ void afisare_b(int v[], int n)
 }
 
 int main()
-{   int i;
+{
+    int i,v2[20];
     ifstream f("atestat.in");
     f>>n;
-    for(i=0;i<n;i++)
+    for(i=0; i<n; i++)
         f>>v[i];
     f.close();
-     //a
-    for(i=0;i<n;i++)
+    //a
+    for(i=0; i<n; i++)
         if(v[i]%2==1)
             cout<<v[i]<<" ";
     cout<<endl;
     //afisez b
-    afisare_b(v,n);
+    afisare_b(v2,n);
     cout<<endl;
     //c
-        int a;
-        a=v[0];
-         while(a) {
+    int a;
+    a=v[0];
+    while(a)
+    {
         int b=a%10;
         s[b]++;
         a=a/10;
-       }
-       int minNr=0;
-       for(int i=9;i>0;i--)
-       {
-           if(s[i]!=0)
-           {
-               minNr=minNr*10;
-               minNr=minNr+i;
-           }
-       }
-       int maxNr=0;
-       for(int i=1;i<9;i++)
-       {
-           if(s[i]!=0)
-           {
-               maxNr=maxNr*10;
-               maxNr=maxNr+i;
-           }
-       }
-       cout<<maxNr<<" "<<minNr;
- return 0;
+    }
+    int minNr=0;
+    for(int i=9; i>0; i--)
+    {
+        if(s[i]!=0)
+        {
+            minNr=minNr*10;
+            minNr=minNr+i;
+        }
+    }
+    int maxNr=0;
+    for(int i=1; i<9; i++)
+    {
+        if(s[i]!=0)
+        {
+            maxNr=maxNr*10;
+            maxNr=maxNr+i;
+        }
+    }
+    cout<<maxNr<<" "<<minNr;
+    return 0;
 }
