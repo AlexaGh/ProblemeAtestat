063b6d74464edfe3f8983d487544e335dee20a0f
AlexaGh <alexa.ghiurau@gmail.com>
2014-03-19 22:10:21 +0200

    uhm2

---
+++ atestat_16/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+using namespace std;
+ifstream f("atestat.in");
+ofstream g("atestat.out");
+
+int main()
+{
+    int i;
+    char a[100];
+    f.get(a,101);
+    //a
+    for(i=0; i<strlen(a); i++)
+    {
+        if(a[i]>=97 && a[i]<=122)
+            a[i]=a[i]-32;
+    }
+    cout<<a;
+    cout<<endl;
+    //b
+    int nr=0;
+    for(i=0; i<strlen(a); i++)
+    {
+        if(a[i]=='A' || a[i]=='E' || a[i]=='I' || a[i]=='O' || a[i]=='U')
+            nr=nr++;
+    }
+    cout<<nr;
+    cout<<endl;
+    //c
+    int contor=0;
+    for(i=0; i<strlen(a)-1; i++)
+    {
+        if(a[i]==32 && a[i+1]=='A')
+            contor=contor++;
+    }
+    g<<contor;
+
+    return 0;
+}
