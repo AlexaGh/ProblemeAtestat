0e5be43be19a1afd0075c254f18878aa28aa1653
AlexaGh <alexa.ghiurau@gmail.com>
2014-03-25 14:00:28 +0200

    # scurtat-c)

--- atestat_18/main.cpp
+++ atestat_18/main.cpp
@@ -34,8 +34,7 @@ void afisare_c()
     int NrOfDigits=0;
     for(i=0; i<strlen(a)-1; i++)
     {
-        if(a[i]=='0' || a[i]=='1' || a[i]=='2' || a[i]=='3' || a[i]=='4'
-           || a[i]=='5' || a[i]=='6' || a[i]=='7' || a[i]=='8' || a[i]=='9')
+        if(a[i]<=57 && a[i]>=48)
             NrOfDigits++;
     }
     g<<NrOfDigits;
